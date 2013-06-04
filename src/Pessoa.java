34196c28c68aaae6fe0ebf517683ec61d68570ed
Gabriella Alves <gabimayara0@gmail.com>
2013-06-04 11:10:30 -0300

    classe pessoa - test

---
+++ src/Pessoa.java
@@ -0,0 +1,38 @@
+public class Pessoa {
+
+	private String nome,  matricula, telefone, cpf;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	
+	
+}
+
+
